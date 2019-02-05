bd419cc1159c3c2608b439d4ec2c8431b9c90af7
Lukasz Piotrowski <lpiotrowski@op.pl>
2019-02-05 21:44:14 +0100

    Init

---
+++ components/app.js
@@ -0,0 +1,10 @@
+const App = React.createClass({
+    render: function() {
+      return (
+        React.createElement('div', {className: 'app'},
+          React.createElement(ContactForm, {contact: contactForm}),
+          React.createElement(Contacts, {items: contacts}, {})
+        )
+      );
+    }
+  });
